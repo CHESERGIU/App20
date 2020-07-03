1eba167c47fc8afc23c9829d055e637c7fc63896
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:54:17 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace App20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            {
+                char first = number.ToString()[0];
+                char second = number.ToString()[1];
+                char third = number.ToString()[2];
+                char fourth = number.ToString()[3];
+                Check(first);                
+                Check(second);
+                Check(third);
+                Check(fourth);
+            }
+        }
+
+        private static void Check(char digit)
+        {
+            switch (digit % 2)
+            {
+                case 0:
+                    Console.WriteLine("PAR");
+                    break;
+                default:
+                    Console.WriteLine("IMPAR");
+                    break;
+            }
+        }
+    }
+}
